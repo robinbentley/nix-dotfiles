db476b2f10bc96550d79c1df478d72944ba5ede7
Robin Bentley <robinbentley@me.com>
2018-08-11 23:36:55 +0100

    Add install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+ln -sfv /home/robin/dotfiles/gemrc /home/robin/.gemrc
+ln -sfv /home/robin/dotfiles/Xmodmap /home/robin/.Xmodmap
+ln -sfv /home/robin/dotfiles/gitconfig /home/robin/.gitconfig
+ln -sfv /home/robin/dotfiles/zshrc /home/robin/.zshrc
+
+mkdir -p /home/robin/.local/share/xfce4/terminal/colorschemes/
+ln -sfv /home/robin/dotfiles/resources/themes/oceanic-next.theme  /home/robin/.local/share/xfce4/terminal/colorschemes/oceanic-next.theme
